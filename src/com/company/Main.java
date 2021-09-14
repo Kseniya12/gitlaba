9e64761363a22506cfa9e09cf2f7bf528c702ff3
Ksenia Nezhivleva <ksyu.nezhivlyova@mail.ru>
2021-09-14 21:55:50 +0300

    func2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,29 @@
+
 package com.company;
+import java.util.Scanner;
 
-public class Main {
+    public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+        public static void main(String[] args) {
+
+                int num1;
+                int num2;
+                String operation;
+
+
+                Scanner input = new Scanner(System.in);
+
+            System.out.println("please enter the first number");
+                num1 =input.nextInt();
+
+            System.out.println("please enter the second number");
+                num2 =input.nextInt();
+
+                Scanner op = new Scanner(System.in);
+
+            System.out.println("Please enter operation");
+                operation =op.next();
+
+        }
     }
-}
+
